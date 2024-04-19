f76dcfbbd8c8ddc8ec48289edac4216ebd33eb7c
rabikohl <116174091+rabikohl@users.noreply.github.com>
2024-04-19 18:23:19 +0200

    Add files via upload

--- LServer.py
+++ LServer.py
@@ -1,15 +1,80 @@
 import socket
 import RPi.GPIO as GPIO
+import smbus		# installieren
+import math
 
-host = "localhost"
-port = 5000
+# Register
+power_mgmt_1 = 0x6b
+power_mgmt_2 = 0x6c
 
-s = socket.socket()
-s.bind((host, port))
-s.listen(1)
-print("Server is listening")
+def read_byte(reg):
+    return bus.read_byte_data(address, reg)
+ 
+def read_word(reg):
+    h = bus.read_byte_data(address, reg)
+    l = bus.read_byte_data(address, reg+1)
+    value = (h << 8) + l
+    return value
+ 
+def read_word_2c(reg):
+    val = read_word(reg)
+    if (val >= 0x8000):
+        return -((65535 - val) + 1)
+    else:
+        return val
+ 
+def dist(a,b):
+    return math.sqrt((a*a)+(b*b))
+ 
+def get_y_rotation(x,y,z):
+    radians = math.atan2(x, dist(y,z))
+    return -math.degrees(radians)
+ 
+def get_x_rotation(x,y,z):
+    radians = math.atan2(y, dist(x,z))
+    return math.degrees(radians)
 
-c, addr = s.accept()
+bus = smbus.SMBus(1) # bus = smbus.SMBus(0) fuer Revision 1
+address = 0x68       # via i2cdetect
+ 
+# Aktivieren, um das Modul ansprechen zu koennen
+bus.write_byte_data(address, power_mgmt_1, 0)
+
+def runAccel(): 
+	print("Gyroskop")
+	print("--------")
+	
+	gyroskop_xout = read_word_2c(0x43)
+	gyroskop_yout = read_word_2c(0x45)   #V02
+	gyroskop_zout = read_word_2c(0x47)
+	
+	B = gyroskop_xout, gyroskop_yout, gyroskop_zout
+      
+	print("gyroskop_xout: "), ("%5d" % gyroskop_xout), (" skaliert: "), (gyroskop_xout / 131)
+	print("gyroskop_yout: "), ("%5d" % gyroskop_yout), (" skaliert: "), (gyroskop_yout / 131)
+	print ("gyroskop_zout: "), ("%5d" % gyroskop_zout), (" skaliert: "), (gyroskop_zout / 131)
+ 
+	print()
+	print("Beschleunigungssensor")
+	print("---------------------")
+ 
+	beschleunigung_xout = read_word_2c(0x3b)
+	beschleunigung_yout = read_word_2c(0x3d)
+	beschleunigung_zout = read_word_2c(0x3f)
+ 
+	beschleunigung_xout_skaliert = beschleunigung_xout / 16384.0
+	beschleunigung_yout_skaliert = beschleunigung_yout / 16384.0
+	beschleunigung_zout_skaliert = beschleunigung_zout / 16384.0
+	
+	A = beschleunigung_xout_skaliert, beschleunigung_yout_skaliert, beschleunigung_zout_skaliert
+	print("beschleunigung_xout: "), ("%6d" % beschleunigung_xout), (" skaliert: "), beschleunigung_xout_skaliert
+	print("beschleunigung_yout: "), ("%6d" % beschleunigung_yout), (" skaliert: "), beschleunigung_yout_skaliert
+	print("beschleunigung_zout: "), ("%6d" % beschleunigung_zout), (" skaliert: "), beschleunigung_zout_skaliert
+ 
+	print("X Rotation: ") , get_x_rotation(beschleunigung_xout_skaliert, beschleunigung_yout_skaliert, beschleunigung_zout_skaliert)
+	print("Y Rotation: ") , get_y_rotation(beschleunigung_xout_skaliert, beschleunigung_yout_skaliert, beschleunigung_zout_skaliert)
+
+	return A, B
 
 
 def valCheckDir(value):
@@ -24,19 +89,30 @@ def valCheckDir(value):
     elif value < 0:
         return 0, (value), abs(value)
     
+host = "localhost"
+port = 5000
+
+s = socket.socket()
+s.bind((host, port))
+s.listen(1)
+print("Server is listening")
+
+c, addr = s.accept()
+
+    
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(12, GPIO.OUT)# PWM 0
 GPIO.setup(13, GPIO.OUT)# PWM
 GPIO.setup(16, GPIO.OUT)
 
 #6, 15, 26
-GPIO.setup(6, GPIO.OUT)
-GPIO.setup(15, GPIO.OUT)
-GPIO.setup(26, GPIO.OUT)
+GPIO.setup(6, GPIO.OUT) #links
+GPIO.setup(15, GPIO.OUT) #rechts
+GPIO.setup(26, GPIO.OUT)    #obenunten
 
-p12 = GPIO.PWM(12, 100)
-p13 = GPIO.PWM(13, 100)
-p16 = GPIO.PWM(16, 100)
+p12 = GPIO.PWM(12, 100) #links
+p13 = GPIO.PWM(13, 100) #rechts
+p16 = GPIO.PWM(16, 100) #hochrunter
 
 motorLeft = 0
 motorRight = 0
